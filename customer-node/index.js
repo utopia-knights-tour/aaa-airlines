b9dc12b7358e7222018b63dad74966a51f3a5d2a
ace <allisterg@gmail.com>
2020-04-29 02:17:44 -0400

    cors

--- customer-node/index.js
+++ customer-node/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -9,6 +10,10 @@ const PORT = process.env.PORT || '3000';
 
 
 const app = express();
+
+app.use(cors());
+app.options('*', cors())
+
 app.use(express.json());
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
